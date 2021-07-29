befdce40dc1f09a00bc8f871e347e89290875a82
mspil88 <matthewspillane88@gmail.com>
2021-07-29 08:46:11 +0100

    added submit functionality and countdown message

--- src/index.js
+++ src/index.js
@@ -1,8 +1,16 @@
+//TODOS
+//(1) no form/ input just modify time on click action
+//(2) message or alert when at zero
+//(3) start one click only
+//(4) stop at zero - DONE
+
+
+
 let secVal = document.getElementById("sec-val");
 let buttons = document.querySelectorAll(".btn");
-
+let secInput = document.getElementById("sec-input");
 let value = parseInt(secVal.textContent);
-const beginningVal = 60;
+const beginningVal = 5;
 console.log(secVal.textContent);
 console.log(buttons);
 console.log(value);
@@ -10,6 +18,9 @@ let t = beginningVal;
 
 let countDown = null;
 
+console.log(secInput);
+
+// check contents of secVal, if stop when boom is blank, cannot reset, fix this
 buttons.forEach((btn) => {
     btn.addEventListener('click', (e) => {
         const actions = e.currentTarget.classList;
@@ -24,19 +35,41 @@ buttons.forEach((btn) => {
         } else if(actions.contains("reset")) {
             console.log("reset clicked");
             countdown = clearInterval(countdown);
+            secVal.innerHTML = beginningVal;
             t = beginningVal;
-            secVal.textContent = beginningVal;
-            
+            if(secVal.innerHTML == '') {
+                console.log('blank');
+            }
+        } else if(actions.contains("submit")) {
+            console.log("submit clicked");
+            console.log(secInput.value);
+            let newInput = parseInt(secInput.value);
+            secVal.innerHTML = newInput;
+            t = newInput;
+
         }
     })
 })
 
 
 function countingDown() {
-    t = --t;
-    console.log(t);
-    secVal.textContent = t;
-}
+    if(t > 0) {
+        t = --t;
+        console.log(t);
+        secVal.innerHTML = t;
+    } else {
+        t = 0;
+        const timeOutMsg = "BOOM";
+        secVal.innerHTML = timeOutMsg;
+        let flash = setTimeout(()=> {
+            secVal.style.color = (secVal.style.color == "black") ? "#84c0ae": "black"
+            console.log("===sec val===")
+            console.log(secVal.innerHTML);
+        }, 50);
+        
+        }
+    }
+
 
 function stopCountDown(interval) {
     clearInterval(interval);
